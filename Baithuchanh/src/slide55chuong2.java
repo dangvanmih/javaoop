d2f65defc1abb9badf174e8636e981b714dac1d7
dangvanmih <minhmeo2k413@gmail.com>
2024-01-27 01:49:04 +0700

    slide55chuong2.java

---
+++ Baithuchanh/src/slide55chuong2.java
@@ -0,0 +1,10 @@
+public class slide55chuong2 {
+
+    public static void main(String[] args) {
+        for(int i = 0 ; i < 20 ; i++)
+        {
+            if(i % 5 == 0)
+            System.out.println(i);
+        }
+    }
+}
