f6e59d89a66698f80634c514940059a3207cd6fb
dangvanmih <minhmeo2k413@gmail.com>
2024-02-02 09:49:53 +0700

    slide74chuong2

---
+++ Baithuchanh/src/slide74chuong2.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class slide74chuong2 {
+    public static void main(String[] args) {
+        int n;
+        double tong = 0;
+        Scanner sc = new Scanner(System.in);
+        do
+        {
+        System.out.print("nhap so phan tu cua mang: ");
+        n = sc.nextInt();
+        }
+        while (n < 0);
+        {
+           int A[] = new int [n];
+           System.out.println("nhap phan tu cho mang: ");
+           for(int i = 0 ; i < n ; i++)
+           {
+            System.out.print("nhap phan tu thu " +i +":");
+            A[i] = sc.nextInt();
+           }
+           for(int i = 0 ; i < n ; i++)
+           {
+            if (A[i] % 2 == 0)
+            {
+                tong += A[i]; 
+            }
+
+           }
+           System.out.print("tong cac so chan tronmg mang la: " + tong);
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+    
+}
