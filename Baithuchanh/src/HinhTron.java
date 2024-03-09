cff2969971dfdde2c542024488ee440a401895b9
dangvanmih <minhmeo2k413@gmail.com>
2024-03-10 02:15:30 +0700

    slide16chuong3

---
+++ Baithuchanh/src/HinhTron.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class HinhTron {
+    // thuộc tính của hình tròn
+    public double banKinh;
+    public double chuVi;
+    public double dienTich;
+    // phương thức
+    public void nhapBanKinh() {
+        System.out.print("Nhap ban kinh: ");
+        Scanner sc = new Scanner(System.in);
+        banKinh = sc.nextDouble();       
+    }
+    public void tinhChuVi() {
+        chuVi = banKinh * 2 * Math.PI; 
+    }
+    public void tinhDienTich() {
+        dienTich = banKinh * banKinh * Math.PI;
+    }
+    public void hienThiKetQua() {
+        System.out.printf("Dien tich = %.3f",dienTich);
+        System.out.println();
+        System.out.printf("Chu vi = %.3f ",chuVi);
+    }
+
+    
+
+
+    
+}
---
+++ Baithuchanh/src/slide16chuong3.java
@@ -0,0 +1,10 @@
+public class slide16chuong3 {
+    public static void main(String[] args) {
+       HinhTron ht = new HinhTron();
+       ht.nhapBanKinh();
+       ht.tinhChuVi();
+       ht.tinhDienTich();
+       ht.hienThiKetQua();
+
+    }
+}
