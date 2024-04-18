0bb3a91efca032048217729a493524836369a495
dangvanmih <minhmeo2k413@gmail.com>
2024-04-18 23:40:52 +0700

    Slide77Chuong3

---
+++ Baithuchanh/src/Slide73Chuong3/MainClass.java
@@ -0,0 +1,14 @@
+package Slide73Chuong3;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Toado td1 = new Toado();
+        td1.x = 10;
+        td1.y = 20;
+        Toado td2 = new Toado();
+        td2.x = 5;
+        td2.y = 6;
+        Toado.thongTin = "Luu toa do cac hinh hoc";
+    }
+    
+}
---
+++ Baithuchanh/src/Slide73Chuong3/Toado.java
@@ -0,0 +1,8 @@
+package Slide73Chuong3;
+
+public class Toado {
+        public static String thongTin;
+        public int x;
+        public int y;
+    
+}
