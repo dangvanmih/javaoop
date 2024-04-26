e5f6403f4fe5dc3ca973fea7faa5bd78c51456cc
dangvanmih <minhmeo2k413@gmail.com>
2024-04-26 09:46:16 +0700

    Slide88-89Chuong3

---
+++ Baithuchanh/src/Slide88Chuong3/Printable.java
@@ -0,0 +1,13 @@
+package Slide88Chuong3;
+interface Printable {
+    void print();
+}
+class A6 implements Printable {
+    public void print() {
+        System.out.println("Hello");
+    }
+    public static void main(String args[]){
+        A6 obj = new A6();
+        obj.print();
+    }
+}
