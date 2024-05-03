12ab970a5c23f8ee1f1d38e30cf47294b3c4aaed
dangvanmih <minhmeo2k413@gmail.com>
2024-05-03 10:21:46 +0700

    Slide21Chuong4

---
+++ Baithuchanh/src/Chuong4/Slide14/Slide21.java
@@ -0,0 +1,20 @@
+package Chuong4.Slide14;
+import java.util.ArrayList;
+
+public class Slide21 {
+    public static void main(String[] args) {
+        ArrayList<String> person = new ArrayList<String>(4);
+        person.add("Tay");
+        person.add("Chan");
+        person.add("Mat");
+        person.add("Mui");
+        person.add("tai");
+
+
+        System.out.println(person.get(1));
+        System.out.println(person.contains("Chan"));
+        System.out.println(person.size());
+        System.out.println(person);
+    }
+    
+}
