95a401cb432d143b4610a97c727670e0276e3e03
dangvanmih <minhmeo2k413@gmail.com>
2024-05-03 10:54:04 +0700

    Slide27Chuong4

---
+++ Baithuchanh/src/Chuong4/Slide14/Slide27.java
@@ -0,0 +1,11 @@
+package Chuong4.Slide14;
+
+import java.util.LinkedList;
+
+public class Slide27 {
+    public static void main(String[] args) {
+        LinkedList<Integer> LinkedList = new LinkedList<>();
+        LinkedList list = new LinkedList<>();
+    }
+    
+}
