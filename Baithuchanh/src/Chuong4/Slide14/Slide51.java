258e6b6c8be1b73d60795b119e0f7d335ea37a0d
dangvanmih <minhmeo2k413@gmail.com>
2024-05-10 10:02:31 +0700

    Slide55Chuong4

---
+++ Baithuchanh/src/Chuong4/Slide14/Slide51.java
@@ -0,0 +1,20 @@
+package Chuong4.Slide14;
+
+import java.util.HashSet;
+import java.util.TreeSet;
+
+public class Slide51 {
+    public static void main(String[] args) {
+        // khai bao mot HashSet co ten la hashSetInt có kiểu là Integer
+        HashSet<Integer> hashSetInt = new HashSet<>();
+        // khai bao mot HashSet co kich thuoc khoi tao = 32
+        HashSet<Character> hashSetChar = new HashSet<>(32);
+        // khai bao HashSet co kich thuoc khoi tao = 16 va yeu to tai = 0.75f(mac dinh)
+        HashSet<String> hashSetString = new HashSet<>(16, 0.75f);
+        // khai bao mot HashSet duoc tao thanh tu mot collection khac
+        HashSet<Float> hashSetFloat = new HashSet<>(new TreeSet<>());
+
+
+    }
+    
+}
---
+++ Baithuchanh/src/Chuong4/Slide14/Slide55.java
@@ -0,0 +1,35 @@
+package Chuong4.Slide14;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Slide55 {
+    public static void main(String[] args) {
+        int number;
+        HashSet<Integer> hashSetInt = new HashSet<>();
+        Scanner sc = new Scanner(System.in);
+        // them cac phan tu vao hashSetInteger
+        hashSetInt.add(0);
+        hashSetInt.add(3);
+        hashSetInt.add(1);
+        hashSetInt.add(4);
+        hashSetInt.add(2);
+        hashSetInt.add(8);
+        // hien thi cac phan tu trong hashSetInteger
+            System.out.println("Cac phan tu trong hashSetIntegr: ");
+            System.out.println(hashSetInt);
+            System.out.println("Nhap phan tu can them: ");
+            number = sc.nextInt();
+            //them mot phan tu moi nhap tu ban phim
+            if(!hashSetInt.contains(number)) {
+                hashSetInt.add(number);
+                System.out.println("Them phan tu thanh cong");
+                System.out.println("Cac phan tu trong hashSetInt sau khi them: " + hashSetInt);
+            }
+            else {
+                System.out.println("Phan tu" + number + "da ton tai");
+            }
+        }
+    }
+    
+
