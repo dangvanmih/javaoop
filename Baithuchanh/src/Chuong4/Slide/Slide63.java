0dac7310523b95b5373cd616fd083f02f156db28
dangvanmih <minhmeo2k413@gmail.com>
2024-05-11 00:12:18 +0700

    Slide63Chuong4

--- Baithuchanh/src/Chuong4/Slide/Slide63.java
+++ Baithuchanh/src/Chuong4/Slide/Slide63.java
@@ -1,4 +1,4 @@
- package Chuong4.Slide14;
+ package Chuong4.Slide;
 
 import java.util.HashSet;
 import java.util.TreeSet;
@@ -8,6 +8,5 @@ class Slide63 {
     public static void main(String[] args) {
         TreeSet<Integer> treeSetIntegers = new TreeSet<>();
         TreeSet<Character> treeSetcCharacters = new TreeSet<>(new HashSet<>());
-        
     }
 }
