4873cf949d0ded5eec2dee41edc0fc1acd0ff414
dangvanmih <minhmeo2k413@gmail.com>
2024-05-24 02:38:16 +0700

    bai2lab4

---
+++ Baithuchanh/src/Chuong4/Slide3/TestStudent.java
@@ -0,0 +1,28 @@
+package Chuong4.Slide3;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TestStudent {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Student> arrayListStudent = new ArrayList<>();
+        System.out.println("Nhap so Phan tu");
+        int n = sc.nextInt();
+        Student.Add(arrayListStudent,n);
+        System.out.println("Danh Sach Sinh Vien:");
+        Student.Display(arrayListStudent);
+
+        Student.themthongtin(arrayListStudent);
+        Student.Display(arrayListStudent);
+
+        Student.Update(arrayListStudent);
+        System.out.println("Danh Sach Sinh Vien Sau Khi Thay Doi");
+        Student.Display(arrayListStudent);
+
+        Student.Delete(arrayListStudent); 
+        System.out.println("Danh Sach Sinh Vien Sau Khi Xoa"); 
+        Student.Display(arrayListStudent);
+    }
+    
+}
