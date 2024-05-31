a594f12e052e2b2188f34d3801efef9fd6aef522
dangvanmih <minhmeo2k413@gmail.com>
2024-05-31 08:52:26 +0700

    BaiKiemTra

---
+++ BaiKiemTra/src/DVM717person.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+;
+
+public interface DVM717person {
+
+    public String FullName;
+    public int Age;
+    public String Address;
+    public void nhapThongTin();
+    public void themThongTin();
+    public void suaThongTin();
+    public void xoaThongTin();
+    public void hienThi();
+
+}
